519fd959745b06a98b81f86f7daa56ed8967d896
aashish-nomad <aashish.nomad@gmail.com>
2025-03-28 14:18:48 +0545

    Initial design setup for homepage - featured job and tag section

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Pixel Positions</title>
+</head>
+<body class="bg-black text-white">
+<div class="px-10">
+    <nav class="flex justify-between items-center py-4 border-b border-white/14">
+        <div>
+            <a href="/">
+                <img src="{{Vite::asset('resources/images/logo.svg')}}" alt="pixel positions logo"/>
+            </a>
+        </div>
+        <div class="space-x-6 font-bold">
+            <a href="">Jobs</a>
+            <a href="">Careers</a>
+            <a href="">Salaries</a>
+            <a href="">Companies</a>
+        </div>
+        <div>
+            <a href="">Post a Job Here</a>
+        </div>
+    </nav>
+    <main class="mt-10 max-w-[986px] mx-auto">{{$slot}}</main>
+</div>
+</body>
+</html>
